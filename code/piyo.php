18cf742de0ce1f46d384d4c7f97a35f2a2ebd124
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-02-08 20:28:22 +0900

    Change only sample HTML

--- code/piyo.php
+++ code/piyo.php
@@ -1,2 +1,11 @@
-<?php
-echo 'This is Piyo.'
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Piyo</title>
+  </head>
+  <body>
+      <?php echo '<h1>This is Piyo</h1>'; ?>
+      <p>Thank you for comming !!</p>
+  </body>
+</html>
